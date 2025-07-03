2d0ca90b15001038114c1ba8b1a19d4847702086
Кузнецова Светлана <144445096+Kuznetsovasveta006@users.noreply.github.com>
2025-07-03 18:57:48 +0300

    Update manager.py

--- src/core/manager.py
+++ src/core/manager.py
@@ -1,11 +1,14 @@
-﻿import random
+import random
 from modules.graph import Graph
 from modules.parameters import Parameters
 from modules.individual import Individual
 from modules.population import Population
 from modules.history import History
 from core.genetic import GeneticAlgorithm
+from gui.utils import RandomGenerator
+import matplotlib.pyplot as plt
 from typing import List, Tuple, Optional
+
 
 class AlgorithmManager:
     def __init__(self):
@@ -17,6 +20,41 @@ class AlgorithmManager:
         self.is_initialized = False
         self.is_completed = False
         
+
+    def load_graph_from_matrix(self, file_path: str) -> None:
+        """
+        Загружает граф из JSON-файла с матрицей смежности
+        Вызывается при нажатии кнопки "Загрузить граф"
+        """
+        try:
+            self.graph = Graph.load_from_matrix_file(file_path)
+        except Exception as e:
+            print(f"Graph loading error: {e}")
+            raise
+
+
+    def generate_random_graph(self, n: int) -> None:
+        """
+        Генерирует случайный неориентированный граф с n вершинами и вероятностью ребра p
+        Вызывается при нажатии кнопки "Сгенерировать случайный граф"
+        """
+        try:
+            matrix = RandomGenerator.generate_random_matrix(n)
+            self.graph = Graph.from_adj_matrix(matrix)
+        except Exception as e:
+            print(f"Graph loading error: {e}")
+            raise
+
+
+    def save_graph_as_matrix(self, file_path: str) -> None:
+        """
+        Сохраняет текущий граф в JSON-файл как матрицу смежности
+        Кнопка "Сохранить граф"
+        """
+        if not self.graph:
+            raise RuntimeError("Graph not loaded")
+        self.graph.save_to_matrix_file(file_path)
+
 
     def set_graph(self, graph: Graph) -> None:
         """Установка графа для алгоритма"""
@@ -30,6 +68,33 @@ class AlgorithmManager:
         self._check_initialization()
 
 
+    def load_and_validate_parameters(self, data: dict) -> None:
+        """
+        Проверяет параметры на корректность
+        Использует graph.n для проверки параметров
+        Кнопка "Загрузить параметры"
+        """
+        if not self.graph:
+            raise RuntimeError("First, download or generate graph")
+
+        self.params = Parameters.from_graph(self.graph.n)
+        # Общая валидация
+        Parameters.validate_parameters(data, self.graph.n)
+
+        # Создание объекта Parameters
+        self.params = Parameters(
+            population_size=data['population_size'],
+            max_generations=data['max_generations'],
+            stagnation_limit=data['stagnation_limit'],
+            max_mutation_prob_gene=float(data['max_mutation_prob_gene']),
+            max_mutation_prob_chrom=float(data['max_mutation_prob_chrom']),
+            fitness_scaling_percent=float(data['fitness_scaling_percent']),
+            max_crossover_points=data['max_crossover_points'],
+            decrease_percent=float(data['decrease_percent']),
+            decrease_step=data['decrease_step'],
+        )
+
+
     def _check_initialization(self) -> None:
         """Проверяет, можно ли инициализировать алгоритм"""
         if self.graph is not None and self.params is not None:
@@ -39,9 +104,9 @@ class AlgorithmManager:
     def initialize_algorithm(self) -> None:
         """Инициализирует алгоритм с текущими графом и параметрами"""
         if self.graph is None:
-            raise RuntimeError("Для выполнения алгоритма должен быть введен граф")
+            raise RuntimeError("To execute algorithm, a graph must be entered")
         if self.params is None:
-            raise RuntimeError("Для выполнения алгоритма должны быть установлены параметры")
+            raise RuntimeError("To execute algorithm, the parameters must be set")
         
         self.algorithm = GeneticAlgorithm(self.graph, self.params)
         self.history = History()
@@ -57,7 +122,7 @@ class AlgorithmManager:
         if not self.is_initialized:
             self.initialize_algorithm()
         if self.is_completed:
-            raise RuntimeError("Алгоритм уже закончил работу")
+            raise RuntimeError("Algorithm has already finished")
 
 
     def step(self) -> Tuple[List[int], List[List[int]]]:
@@ -89,19 +154,18 @@ class AlgorithmManager:
 
         return self._get_current_state()
 
+
     def run_until_completion(self) -> Tuple[List[int], List[List[int]]]:
         """Выполняет алгоритм до завершения"""
         self._check_ready()
         
         while not self.algorithm.should_stop():
             self.algorithm.next_generation()
-            self.history.record(
-                self.algorithm.generation,
-                self.algorithm.population
-            )
+            self.history.record(self.algorithm.population)
         
         self.is_completed = True
         return self._get_current_state()
+
 
     def _get_current_state(self) -> Tuple[List[int], List[List[int]]]:
         """Возвращает текущее состояние алгоритма"""
@@ -112,4 +176,21 @@ class AlgorithmManager:
         population = self.algorithm.get_population_chromosomes()
         
         return best_solution, population
+
+
+    def plot_history(self) -> plt.Figure:
+        """
+        Строит график эволюции на основе истории.
+        Предполагается, что History имеет метод plot(),
+        возвращающий объект matplotlib.figure.Figure.
+        """
+        if self.history is None:
+            raise RuntimeError("History is not initialized")
+        try:
+            fig = self.history.plot_fitness()
+        except Exception as e:
+            print(f"Error plotting history: {e}")
+            raise
+        return fig
+
     
