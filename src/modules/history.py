7f156807b0e82618381b6b44bf168d507b54e791
Кузнецова Светлана <144445096+Kuznetsovasveta006@users.noreply.github.com>
2025-07-03 21:01:43 +0300

    Update history.py

--- src/modules/history.py
+++ src/modules/history.py
@@ -1,43 +1,41 @@
 from population import Population
+from individual import Individual
 import matplotlib.pyplot as plt
 import json
 
 class History:
     def __init__(self):
-        self.generations: list[int] = []            # Список номеров поколений
         self.best_fitness: list[float] = []         # Лучшие приспособленности на каждом поколении
         self.avg_fitness: list[float] = []          # Средние приспособленности на каждом поколении
-        self.best_solutions: list[list[int]] = []   # Лучшие решения на каждом поколении (хромосомы)
 
 
-    def record(self, generation: int, population: Population):
+    def record(self, population: Population):
         """Пересчитывает параметры популяции и сохраняет их в историю"""
         population.update_stats()
-        self.generations.append(generation)
         self.best_fitness.append(population.best.fitness)
         self.avg_fitness.append(population.avg_fitness)
-        self.best_solutions.append(population.best.chromosome.copy())
 
 
-    def plot_fitness(self):
-        """Строит график динамики лучшей и средней приспособленности по поколениям."""
-        plt.figure()
-        plt.plot(self.generations, self.best_fitness, label='Best Fitness')
-        plt.plot(self.generations, self.avg_fitness, label='Average Fitness')
-        plt.xlabel('Generation')
-        plt.ylabel('Fitness')
-        plt.legend()
-        plt.grid(True)
-        plt.show()
+    def plot_fitness(self) -> plt.Figure:
+        """Строит и возвращает график динамики лучшей и средней приспособленности по поколениям"""
+        generations = list(range(len(self.best_fitness)))
+        fig, ax = plt.subplots()
+
+        ax.plot(generations, self.best_fitness, label='Best Fitness')
+        ax.plot(generations, self.avg_fitness, label='Average Fitness')
+        ax.set_xlabel('Generation')
+        ax.set_ylabel('Fitness')
+        ax.legend()
+        ax.grid(True)
+
+        return fig
 
 
     def save_to_json(self, path: str):
-        """ Сохраняет историю работы алгоритма в результирующий json-файл"""
+        """Сохраняет историю работы алгоритма в результирующий json-файл"""
         data = {
-            'generations': self.generations,
             'best_fitness': self.best_fitness,
-            'avg_fitness': self.avg_fitness,
-            'best_solutions': self.best_solutions
+            'avg_fitness': self.avg_fitness
         }
         with open(path, 'w', encoding='utf-8') as f:
             json.dump(data, f, indent=2)
